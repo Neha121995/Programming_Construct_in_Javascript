52e9948c44e1bc469ea9f9ee9b01931e8360ff7e
Neha121995 <jagtapneha1212@gmail.com>
2021-08-24 00:21:54 +0530

    [Neha] added: Javascript Programming Construct

---
+++ Selection/CaseStmtProblem/SingleDigitNoToWord.js
@@ -0,0 +1,26 @@
+var number = 10;
+
+switch(number)
+{
+    case 1:
+        console.log("One");
+        break;
+    case 10:
+        console.log("Ten");
+        break;
+    case 100:
+        console.log("Hundred");
+        break;
+    case 1000:
+        console.log("Thousand");
+        break;
+    case 10000:
+        console.log("Ten thousand");
+        break;
+    case 100000:
+        console.log("1 Lakh");
+        break;
+    default:
+        console.log("You've entered wrong input.");
+        break;
+}
---
+++ Selection/CaseStmtProblem/WeekDaySwitchCase.js
@@ -0,0 +1,28 @@
+var number = 3;
+switch(number)
+{
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("you've entered out of range number.");
+        break;
+}
